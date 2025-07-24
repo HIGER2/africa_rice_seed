0edeca25f7e60d094e86aaa5e086450c6c0953e9
douma <doumaarmamd@gmail.com>
2025-07-24 13:59:23 +0000

    modif

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -237,6 +237,7 @@ onMounted(() => {
                 <div v-if="payload.order.seed_class === 'AfricaRice'" class="flex-1">
                   <label class="block text-sm font-medium text-gray-600 mb-1">Variety Type</label>
                   <select @change="(event) => selectVariete(event, index)"
+                        value=""
                           class="w-full text-sm border border-gray-300 rounded-lg px-3 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
                     <option disabled value="">Variety Type</option>
                     <option v-for="(item, index) in type" :value="JSON.stringify(item)" :key="index">
